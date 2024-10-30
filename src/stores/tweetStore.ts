ed7627233e91ec7d446d3e5830950f0f3e461b0a
cfa532 <cfa532@yahoo.com>
2024-10-30 09:34:19 +0800

    Update tweetStore.ts

--- src/stores/tweetStore.ts
+++ src/stores/tweetStore.ts
@@ -1,5 +1,6 @@
 import { defineStore } from 'pinia';
 import { useLeitherStore } from './leitherStore';
+const GUEST_ID = "000000000000000000000000000"
 
 export const useTweetStore = defineStore('tweetStore', {
     state: () => ({
@@ -71,7 +72,7 @@ export const useTweetStore = defineStore('tweetStore', {
                 userid: author.mid,
                 start: Date.now(),
                 end: Date.now() - 2592000000,
-                gid: "000000000000000000000000000"      // visitor's mid
+                gid: GUEST_ID      // visitor's mid
             })
         },
 
@@ -79,7 +80,6 @@ export const useTweetStore = defineStore('tweetStore', {
         async getTweet(tweetId: string, authorId: string | undefined = undefined): Promise<Tweet | undefined> {
             let tweet = this.tweets.find(t => { t.mid == tweetId })
             if (tweet) {
-                console.log(tweet)
                 return tweet
             }
             tweet = await this.fetchTweet(tweetId)
@@ -94,11 +94,12 @@ export const useTweetStore = defineStore('tweetStore', {
             return tweet
         },
 
-        // Given tweet ID, get its content. There are 2 steps. First, find provider of
-        // this tweet with its ID. 2nd, retrieve the tweet from the provider. Assume
-        // author data is also available on the provider. Get author data too.
+        /**
+         * Given tweet ID, get its content. There are 2 steps. First, find provider of
+         * this tweet with its ID. 2nd, retrieve the tweet from the provider. Assume
+         * author data is also available on the provider. Get author data too.
+         */
         async fetchTweet(tweetId: string): Promise<Tweet | undefined> {
-
             // Get IP address of the provider of this tweet
             let providerIp = await this.getProviderIp(this.lapi.client, tweetId)
             if (!providerIp) return
@@ -109,7 +110,7 @@ export const useTweetStore = defineStore('tweetStore', {
                 aid: this.lapi.appId,
                 ver: "last",
                 tweetid: tweetId,
-                userid: "0000000000000000000000000000000",  // just a placeholder
+                userid: GUEST_ID,  // just a placeholder
             })
             console.log(tweetInDB)
             // get author data of the tweet.
@@ -177,7 +178,7 @@ export const useTweetStore = defineStore('tweetStore', {
                 aid: this.lapi.appId,
                 ver: "last",
                 tweetid: tweet.mid,
-                userid: "00000000000000000000000000",
+                userid: GUEST_ID,
             }) as any[]
             // comment type if a different Tweet type from the definition in this app
             comments.forEach(async e => {
