3e57f95009f27762415770384da1219c9c11613a
cfa532 <cfa532@yahoo.com>
2024-12-11 16:31:49 +0800

    standalone QR modal

--- src/views/DetailHeader.vue
+++ src/views/DetailHeader.vue
@@ -17,12 +17,7 @@ const tweetStore = useTweetStore();
 const router = useRouter();
 const downloadApk = import.meta.env.VITE_APP_PKG
 const dlUrl = ref();
-const qrSize = 80;
-const showModal = ref(false);
-
-function toggleModal() {
-  showModal.value = !showModal.value;
-}
+const qrSize = ref(60);
 
 const avatar = computed(() => {
   let url = 'http://' + props.author.providerIp;
@@ -47,17 +42,14 @@ function openUserPage(userId: string) {
       <div class='avatar me-2'>
         <img :src='avatar' alt='User Avatar' class='rounded-circle' @click.stop='openUserPage(author.mid)'>
       </div>
-      <!-- User Info -->
       <div class='user-info flex-grow-1'>
         <div v-if='isRetweet' class='label text-muted small'>
           Forwarded by @{{ by }}
         </div>
-        <!-- Username, Alias, and Time -->
         <div class='username-alias-time'>
           <span class='username fw-bold'>{{ author.name }}</span>
           <span class='alias text-muted'>@{{ author.username }}</span>
         </div>
-        <!-- Followers and Friends Links -->
         <div class='mt-1'>
           <span v-if='props.timestamp' class='time text-muted'>{{ formatTimeDifference(props.timestamp as number) }}</span>
         </div>
@@ -65,15 +57,9 @@ function openUserPage(userId: string) {
     </div>
     <div class='d-flex align-items-end'>
       <button class='btn btn-link' @click='tweetStore.downloadApk'>APP ⬇️</button>
-      <div class='qr-code-container' @click='toggleModal'>
+      <div class='qr-code-container'>
         <QRCoder v-if='dlUrl' :url='dlUrl' :size='qrSize'></QRCoder>
       </div>
-    </div>
-  </div>
-  <!-- modal window to show large QR code -->
-  <div v-if='showModal' class='modal-overlay' @click='toggleModal'>
-    <div class='modal-content' @click.stop>
-      <QRCoder :url='dlUrl' :size='190' :logoSize="40"></QRCoder>
     </div>
   </div>
 </template>
@@ -83,12 +69,11 @@ function openUserPage(userId: string) {
   font-size: 0.8rem;
 }
 .qr-code-container {
-  width: 60px;
-  height: 60px;
   display: flex;
   justify-content: center;
   align-items: center;
 }
+
 .username {
   font-size: 0.9rem;
 }
@@ -101,8 +86,8 @@ function openUserPage(userId: string) {
 }
 .avatar img {
   object-fit: cover;
-  width: 40px;
-  height: 40px;
+  width: 50px;
+  height: 50px;
   cursor: pointer;
 }
 .user-info {
@@ -116,26 +101,5 @@ function openUserPage(userId: string) {
 }
 .links a:hover {
   text-decoration: underline;
-}
-.modal-overlay {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background: rgba(0, 0, 0, 0.5);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-.modal-content {
-  background: white;
-  width: 200px; /* Adjust size as needed */
-  height: 200px; /* Make it a square */
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border-radius: 8px;
-  position: relative;
 }
 </style>
--- src/views/QRCoder.vue
+++ src/views/QRCoder.vue
@@ -1,71 +1,100 @@
 <script setup lang="ts">
-import { onMounted, ref } from "vue";
+import { ref } from "vue";
+import QrcodeVue from 'qrcode.vue';
 import { useTweetStore } from "@/stores";
-import QrcodeVue from 'qrcode.vue'
-import type { Level, RenderAs, GradientType, ImageSettings } from 'qrcode.vue'
+import type { Level, RenderAs, GradientType, ImageSettings } from 'qrcode.vue';
 
 const props = defineProps({
-  url: { type: String, required: true },
-  size: { type: Number, required: false },
+  url: { type: String, required: true },  // QR data url
+  size: { type: Number, required: false, default: 100 },
   logoSize: { type: Number, required: false, default: 20 },
-})
+});
 
-const level = ref<Level>('M')
-const renderAs = ref<RenderAs>('svg')
-const background = ref('#ffffff')
-const foreground = ref('#604060')
-const margin = ref(0)
+const level = ref<Level>('M');
+const renderAs = ref<RenderAs>('svg');
+const background = ref('#ffffff');
+const foreground = ref('#604060');
+const margin = ref(0);
+const logoUrl = ref("http://" + useTweetStore().lapi.hostIP + "/mm/xmzaZPI_0CHL4hWGJukqC6yyGyW")
 
-const imageSettings = ref<ImageSettings>({
-  src: "http://" + useTweetStore().lapi.hostIP + "/mm/xmzaZPI_0CHL4hWGJukqC6yyGyW",
+const logoSettings = ref<ImageSettings>({
+  src: logoUrl.value,
   width: props.logoSize,
   height: props.logoSize,
-  // x: 10,   // x-position of the logo
-  // y: 10,
   excavate: false,
-})
-
-const gradient = ref(false)
-const gradientType = ref<GradientType>('radial')
-const gradientStartColor = ref('#6a4b6f')
-const gradientEndColor = ref('#000000')
-onMounted(() => {
 });
+const largeLogoSettings = ref<ImageSettings>({
+  src: logoUrl.value,
+  width: props.logoSize *2,
+  height: props.logoSize *2,
+  excavate: false,
+});
+
+const gradient = ref(false);
+const gradientType = ref<GradientType>('radial');
+const gradientStartColor = ref('#6a4b6f');
+const gradientEndColor = ref('#000000');
+
+const showModal = ref(false);
+const largeQRSize = ref(200)
 </script>
 
 <template>
-  <qrcode-vue
-    :value=props.url
-    :size=props.size
-    :level="level"
-    :render-as="renderAs"
-    :background="background"
-    :foreground="foreground"
-    :gradient="gradient"
-    :gradient-type="gradientType"
-    :gradient-start-color="gradientStartColor"
-    :gradient-end-color="gradientEndColor"
-    :image-settings="imageSettings"
-  />
+  <div class="qrcode-container" @click="showModal = true">
+    <qrcode-vue
+      :value="props.url"
+      :size="props.size"
+      :level="level"
+      :render-as="renderAs"
+      :background="background"
+      :foreground="foreground"
+      :gradient="gradient"
+      :gradient-type="gradientType"
+      :gradient-start-color="gradientStartColor"
+      :gradient-end-color="gradientEndColor"
+      :image-settings="logoSettings"
+    />
+  </div>
+
+  <div v-if="showModal" class="modal" @click="showModal = false">
+    <div class="modal-content" @click.stop>
+      <qrcode-vue
+        :value="props.url"
+        :size=largeQRSize
+        level="H"
+        render-as="svg"
+        background="#ffffff"
+        foreground="#604060"
+        :image-settings="largeLogoSettings"
+      />
+    </div>
+  </div>
 </template>
 
 <style scoped>
 .qrcode-container {
   position: relative;
-  width: 256px;
-  height: 256px;
+  cursor: pointer;
 }
 
-.logo {
-  background-color: rgb(72, 76, 84);
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  width: 50px;
-  /* Adjust the size of the logo */
-  height: 50px;
-  display: none;
-  /* Hide the image element */
+.modal {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.modal-content {
+  background-color: white;
+  padding: 10px;
+  width: 220px;
+  height: 220px;
+  border-radius: 8px;
+  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
 }
 </style>
